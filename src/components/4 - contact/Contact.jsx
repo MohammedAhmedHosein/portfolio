1975263428505b62dcb1a2367e91ad441aea9128
Mohammed Ahmed <m.ahmadhosein67@gmail.com>
2024-11-28 09:12:11 +0400

    Contact us section implementation

--- src/components/4 - contact/Contact.jsx
+++ src/components/4 - contact/Contact.jsx
@@ -1,6 +1,33 @@
 import "./contact.css";
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section className="contact-us">
+      <h1 className="title">
+        <span className="icon-envelope"> </span>Contact us
+      </h1>
+      <p className="sub-title">
+        Contact us for more information and get notified when i publish
+        something new{" "}
+      </p>
+
+      <div className="flex">
+        <form className="">
+          <div className="flex">
+            <label htmlFor="email">Email Address :</label>
+            <input type="email" id="email" required />
+          </div>
+          <div className="flex" style={{ marginTop: "24px" }}>
+            <label htmlFor="message">Your Message :</label>
+            <textarea id="message" required></textarea>
+          </div>
+          <button type="submit" className="submit">
+            Submit
+          </button>
+        </form>
+        <div className="border animation">animation</div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
